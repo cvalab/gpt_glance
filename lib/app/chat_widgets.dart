64a1078fc94e4cabd03c185758c88eb494afdbf9
Good-rice-soup <varnachevlev@gmail.com>
2024-07-05 19:53:07 +0400

    refactor: submitUserMessage connected with api_service.dart + UI changed

--- lib/app/chat_widgets.dart
+++ lib/app/chat_widgets.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../api_service.dart';
 import '../app_states.dart';
 import '../chat_repository.dart';
 import '../message_repository.dart';
@@ -36,8 +37,9 @@ class _ChatWidgets extends State<ChatWidgets> {
   late ChatRepository chatRep;
   late MessageRepository messageRep;
   List<Map<String, dynamic>> messages = [];
-  int chatId = -1;
+  int chatId = 0;
   String chatName = '';
+  bool isChatRenamed = false;
 
   @override
   void initState() {
@@ -53,6 +55,7 @@ class _ChatWidgets extends State<ChatWidgets> {
     chatId = appState.chatId;
     chatName = appState.chatName;
     messages = appState.messages;
+    isChatRenamed = appState.isRenamed;
     if (chatId != 0) {
       _loadMessages();
     }
@@ -71,7 +74,7 @@ class _ChatWidgets extends State<ChatWidgets> {
           chatId: chatId, message: message, isBot: false);
       await _loadMessages();
     } else {
-      const String name = '---test name---';
+      const String name = ' ';
       final int id = await chatRep.createChat(name);
       await messageRep.addMessage(chatId: id, message: message, isBot: false);
       final chatList = await chatRep.getChats();
@@ -84,11 +87,46 @@ class _ChatWidgets extends State<ChatWidgets> {
           chatId: id,
           chatName: name,
           messages: messages,
+          isRenamed: 0,
         );
         if (chatId != 0) {
           await _loadMessages();
         }
       }
+    }
+
+    final chatGptApiService =
+        Provider.of<ChatGptApiService>(context, listen: false);
+    try {
+      final response = await chatGptApiService.sendMessage(message);
+      await messageRep.addMessage(
+          chatId: chatId, message: response, isBot: true);
+      await _loadMessages();
+
+      if (isChatRenamed) {
+        final newChatNameResponse = await chatGptApiService.sendMessage(
+            "Please suggest an interesting name for chat by it's first message. "
+            "Don't forget about wight spaces between words, and write it as a small sentence. "
+            'WRITE ONLY NEW CHAT NAME AND NOTHING MORE. Message: <<$message>>');
+        final newChatName = newChatNameResponse.trim();
+        await chatRep.renameChat(chatId, newChatName);
+        chatName = newChatName;
+        Provider.of<LoadChatMessages>(context, listen: false)
+            .transferLoadingData(
+          chatId: chatId,
+          chatName: newChatName,
+          messages: messages,
+          isRenamed: 1,
+        );
+
+        final chatList = await chatRep.getChats();
+        final List<Map<String, dynamic>> listOfChats =
+            chatList.reversed.toList();
+        Provider.of<LoadChats>(context, listen: false)
+            .updateListOfChats(listOfChats: listOfChats);
+      }
+    } on Exception catch (e) {
+      print('Error: $e');
     }
   }
 
@@ -114,42 +152,57 @@ class _ChatWidgets extends State<ChatWidgets> {
                   slivers: [
                     SliverList(
                       delegate: SliverChildBuilderDelegate(
-                            (context, index) {
+                        (context, index) {
                           return Align(
                             alignment: messages[index]['is_bot'] == 0
                                 ? Alignment.centerRight
                                 : Alignment.centerLeft,
                             child: Padding(
                               padding: EdgeInsets.only(
-                                left: messages[index]['is_bot'] == 0 ? 40.0 : 5.0,
-                                right: messages[index]['is_bot'] == 1 ? 40.0 : 5.0,
+                                left:
+                                    messages[index]['is_bot'] == 0 ? 40.0 : 5.0,
+                                right:
+                                    messages[index]['is_bot'] == 1 ? 40.0 : 5.0,
                               ),
                               child: ConstrainedBox(
                                 constraints: BoxConstraints(
-                                  maxWidth: MediaQuery.of(context).size.width * 0.85,
+                                  maxWidth:
+                                      MediaQuery.of(context).size.width * 0.85,
                                 ),
                                 child: Card(
-                                  color: Theme.of(context).colorScheme.tertiaryContainer,
+                                  color: messages[index]['is_bot'] == 0
+                                      ? Theme.of(context)
+                                          .colorScheme
+                                          .secondaryContainer
+                                      : Theme.of(context)
+                                          .colorScheme
+                                          .tertiaryContainer,
                                   shape: RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(10),
                                   ),
                                   child: Padding(
                                     padding: const EdgeInsets.all(10),
                                     child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.end,
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
                                       children: [
                                         Text(
                                           messages[index]['text'],
                                           style: TextStyle(
-                                            color: Theme.of(context).colorScheme.primary,
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primary,
                                             fontSize: 15,
                                           ),
                                         ),
                                         Text(
                                           messages[index]['time'],
                                           style: TextStyle(
-                                            color: Theme.of(context).colorScheme.primary,
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primary,
                                             fontSize: 13,
                                           ),
                                         ),
